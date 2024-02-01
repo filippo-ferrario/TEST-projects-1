92f46672e7b8cbf41bace589cc104f436634817c
Ferrario <filippo.f3rrario@gmail.com>
2024-02-01 10:33:00 -0500

    added FF tag  to ezispin calls

--- telemBC/R_workflow/FF-0-main-telemBC.R
+++ telemBC/R_workflow/FF-0-main-telemBC.R
@@ -44,5 +44,5 @@ library(ezknitr)
 # example on how parallize you code
 # -----------------------------------
 # input: none
-ezspin(file='R_workflow/example-parallel_processing.R', keep_md=FALSE,  out_dir='R_output/Rmarkdown')
-# input: /R_output/Rmarkdown/example-parallel_processing.html
+ezspin(file='R_workflow/FF-example-parallel_processing.R', keep_md=FALSE,  out_dir='R_output/Rmarkdown')
+# input: /R_output/Rmarkdown/FF-example-parallel_processing.html
